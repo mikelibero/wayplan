d8e81d92b91e2844b18e1e99eca9954f7da0462d
Mikel <mikelibero@mail.com>
2023-03-13 23:06:27 +0100

    revalidate con token

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,18 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-  // await res.revalidate("/");
-  // return res.json({ revalidated: true })
-
-  // if (req.headers['x-secret'] !== process.env.MY_SECRET_TOKEN) {
-  //   return res.status(401).json({ message: 'Invalid token' })
-  // }
-  // try {
-  //   await res.revalidate("/")
-  //   return res.json({ revalidated: true })
-  // } catch (err) {
-  //   return res.status(500).send('Error revalidating')
-  // }
+  if (req.headers['x-secret'] !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+  try {
+    await res.revalidate("/")
+    return res.json({ revalidated: true })
+  } catch (err) {
+    return res.status(500).send('Error revalidating')
+  }
 }
