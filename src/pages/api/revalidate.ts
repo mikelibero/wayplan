169401eaffa0059d85c7272755a9472f489811dd
Mikel <mikelibero@mail.com>
2023-03-13 19:21:33 +0100

    Cambio revalidate

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -2,11 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  return res.json({ revalidated: true })
+
   // if (req.headers['x-secret'] !== process.env.MY_SECRET_TOKEN) {
   //   return res.status(401).json({ message: 'Invalid token' })
   // }
   // try {
-  //   await res.revalidate(req.query.path)
+  //   await res.revalidate("/")
   //   return res.json({ revalidated: true })
   // } catch (err) {
   //   return res.status(500).send('Error revalidating')
